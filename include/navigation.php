20e09c127499cb52cf397d671df9cf23e12dde45
@VtechROshan <vtech.rooshan@gmail.com>
2023-04-29 18:08:24 +0545

    Final_Suraj Dai

---
+++ include/navigation.php
@@ -0,0 +1,61 @@
+
+
+<body>
+    <!-- Topbar Start -->
+    <div class="container-fluid px- d-none d-sm-block">
+        <div class="row   align-items-center">
+            <div class="col-lg-3">
+                <div class="d-flex align-items-center justify-content-start">
+                    <i class="bi bi-phone-vibrate fa-lg text-primary me-2"></i>
+                    <p class="mb-0"> <b>+977 985-6054211 </b></p>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="d-flex align-items-center justify-content-center">
+                    <a href="index.php" class="navbar-brand ms-lg-5">
+                        <h5 class="m-0  display-6 text-primary"><span class="text-secondary"> <strong>Garadi</strong> <b> Agro </span> & Durbar Cafe</b></h5>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="d-flex align-items-center justify-content-end">
+                    <a class="btn-sm btn-primary btn-square rounded-circle me-2" href="#"><i class="fab fa-twitter"></i></a>
+                    <a class="btn-sm btn-primary btn-square rounded-circle me-2" href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a class="btn-sm btn-primary btn-square rounded-circle me-2" href="#"><i class="fab fa-linkedin-in"></i></a>
+                    <a class="btn-sm btn-primary btn-square rounded-circle" href="#"><i class="fab fa-instagram"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Topbar End -->
+
+
+    <!-- Navbar Start -->
+    <nav class="navbar navbar-expand-lg bg-primary navbar-dark shadow-sm py-1 py-lg-0 px-3 px-lg-1">
+        <a href="index.php" class="navbar-brand d-flex d-lg-none">
+            <h1 class="m-0 display-4 text-secondary"><span class="text-white">Farm</span>Fresh</h1>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav mx-auto py-0">
+                <a href="index.php" class="nav-item nav-link active">Home</a>
+                <a href="about.php" class="nav-item nav-link">About</a>
+                <a href="service.php" class="nav-item nav-link">Service</a>
+                <a href="product.php" class="nav-item nav-link">Product</a>
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                    <div class="dropdown-menu m-0">
+                        <a href="blog.php" class="dropdown-item">Blog Grid</a>
+                        <a href="detail.php" class="dropdown-item">Blog Detail</a>
+                        <a href="feature.php" class="dropdown-item">Features</a>
+                        <a href="team.php" class="dropdown-item">The Team</a>
+                        <a href="testimonial.php" class="dropdown-item">Testimonial</a>
+                    </div>
+                </div>
+                <a href="contact.php" class="nav-item nav-link">Contact</a>
+            </div>
+        </div>
+    </nav>
+    <!-- Navbar End -->
---
+++ product.php
@@ -0,0 +1,11 @@
+<?php 
+include("./include/header.php");
+include("./include/navigation.php");
+?>
+
+
+
+    
+<?php 
+include("./include/footer.php");
+?>
